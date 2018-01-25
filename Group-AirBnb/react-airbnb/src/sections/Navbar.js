b5b5a8ed6fb2c39eef29b2f050c47779a607e29d
munimuni5”git config --global user.name munimuni5” <emileeleamons@gmail.com>
2018-01-25 15:36:18 -0800

    sharing is caring

---
+++ Group-AirBnb/react-airbnb/src/sections/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import { Button, Card, Row, Col } from 'react-materialize';
+
+// Navbar
+
+class Navbar extends Component {
+  render () {
+
+    return (
+      <Navbar brand='logo' right>
+        <a href="#"> Become a host</a>
+        <a href="#"> Saved</a>
+        <a href="#"> Saved</a>
+        <a href="#"> Saved</a>
+        <a href="#"> Saved</a>
+      </Navbar>
+    )
+  }
+}
+
+export default Navbar;
